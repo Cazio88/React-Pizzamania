4e24a77de45bc684987d94202aabe7509d9fc696
Francis Mensah <kophy.fm@mail.com>
2023-04-16 06:42:54 -0700

    added git to pizza website in react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+import Carousel from './Carousel'
+import About from './About'
+import Types from './Types'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+      <About />
+      <Types />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Types.js
@@ -0,0 +1,65 @@
+function Types(){
+    return(
+        <div className="container-fluid pizza-types">
+    <div className="row justify-content-center">
+      <h3>Pizza Types</h3>
+    </div>
+  <div className="row">
+    <div className="col-md-3">
+      <div className="card" style={{width: '18rem'}} >
+        <img src="images/8.jpg" className="card-img-top" alt="Pizza" />
+      <div className="card-body">
+        <h5 className="card-title">Tasty Pizaa</h5>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        .</p>
+      </div>
+      </div>
+    </div>
+    <div className="col-md-3">
+      <div className="card" style={{width: '18rem'}} >
+        <img src="images/6.jpg" className="card-img-top" alt="Pizza" />
+  <div className="card-body">
+        <h5 className="card-title">Sausage Pizaa</h5>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        .</p>
+  </div>
+    </div>
+
+  </div>
+    <div className="col-md-3">
+      <div className="card" style={{width: '18rem'}} >
+        <img src="images/9.jpg" className="card-img-top" alt="Pizza" />
+  <div className="card-body">
+        <h5 className="card-title">Peperoni Pizaa</h5>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        </p>
+  </div>
+
+    </div>
+  </div>
+    <div className="col-md-3">
+      <div className="card" style={{width: '18rem'}} >
+        <img src="images/7.jpg" className="card-img-top" alt="Pizza" />
+  <div className="card-body">
+        <h5 className="card-title">Large Pizza</h5>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        
+        .</p>
+  </div>
+
+    </div>
+    </div>
+  </div>
+</div>
+    )
+}
+
+export default Types;
